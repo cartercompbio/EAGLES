cccc145575f4b4e5157a8c0971698cb5fffbb054
Douglas-Meyer <113216623+Douglas-Meyer@users.noreply.github.com>
2025-12-02 12:22:24 -0800

    Create _dep_model_score.py

---
+++ v0.2/bin/_dep_model_score.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import joblib
+import argparse
+from fit_model import load_covariates
+
+#def load_covariates(path):
+#    cov = pd.read_csv(path, sep="\t")
+#    if "#IID" in cov.columns:
+#        cov = cov.rename(columns={"#IID": "IID"})
+#    cov = cov.set_index("IID")
+#    cov.index = cov.index.astype(str).str.strip()
+#    return cov
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--features", required=True)
+    parser.add_argument("--model", required=True)
+    parser.add_argument("--covariates", required=True)
+    parser.add_argument("--output", required=True)
+    args = parser.parse_args()
+
+    # Load features
+    X = pd.read_csv(args.features, sep="\t", index_col=0)
+
+    # Load covariates
+    cov = pd.read_csv(args.covariates, sep="\t")
+    if "#IID" in cov.columns:
+        cov = cov.rename(columns={"#IID": "IID"})
+    cov = cov.set_index("IID")
+    cov.index = cov.index.astype(str).str.strip()
+
+    # Align features and covariates
+    common_samples = X.index.intersection(cov.index)
+    X = X.loc[common_samples]
+    cov = cov.loc[common_samples]
+
+    # Concatenate covariates
+    X = pd.concat([X, cov], axis=1)
+
+    # Load model
+    model_dict = joblib.load(args.model)
+
+    # Hanlge flipped allele "score"
+    if "flip_mask" in model_dict:
+        # Only keep SNPs (ignore covariates)
+        snp_cols = model_dict["feature_names"]
+        X_snps = X[snp_cols].copy() 
+    
+        # Sum alleles per sample
+        y_pred = X_snps.sum(axis=1).astype(int)
+    
+        output_df = pd.DataFrame({
+            "sample_id": X_snps.index,
+            "flipped_allele_sum": y_pred
+        }).set_index("sample_id")
+
+
+    # Normal ML models
+    else:
+        scaler = model_dict["scaler"]
+        model = model_dict["model"]
+        feature_names = model_dict["feature_names"]
+
+        # Ensure all features present
+        missing = set(feature_names) - set(X.columns)
+        if missing:
+            raise ValueError(f"Missing features in input data: {missing}")
+
+        # Order columns
+        X_ordered = X[feature_names]
+
+        # Scale and predict
+        X_scaled = scaler.transform(X_ordered)
+        y_pred = model.predict(X_scaled)
+
+        output_df = pd.DataFrame({
+            "sample_id": X_ordered.index,
+            "predicted_expression": y_pred
+        }).set_index("sample_id")    
+    
+    output_df.to_csv(args.output, sep="\t")
+
+
+if __name__ == "__main__":
+    main()
